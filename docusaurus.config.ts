9377da8c1bef20c670f2baefc18b3c8d0bd1b52a
Marcel Menk <marcel.menk@forepath.io>
2025-07-17 20:40:04 +0200

    fix: organization name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'kublade', // Usually your GitHub org/user name.
+  organizationName: 'forepath', // Usually your GitHub org/user name.
   projectName: 'kublade', // Usually your repo name.
 
   onBrokenLinks: 'throw',
